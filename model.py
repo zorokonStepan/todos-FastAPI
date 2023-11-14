03cb39f1d6f81449883ccff83c2069612794535c
SKolodkin <dev1@instrument-fit.ru>
2023-11-14 09:04:51 +0300

    create async def get_single_todo

--- model.py
+++ model.py
@@ -4,3 +4,22 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+    class Config:
+        json_schema_extra = {
+            "Example": {
+                "id": 1,
+                "item": "Example schema!"
+            }
+        }
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
