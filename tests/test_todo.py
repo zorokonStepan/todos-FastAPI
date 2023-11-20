52b52c924478b21bf9c251b33b63567f68a50f3c
SKolodkin <dev1@instrument-fit.ru>
2023-11-20 15:19:55 +0300

    create TestTodo with __request, test_retrieve_todos, test_add_todo

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,9 +1,13 @@
 import requests
 
+from json import dumps
+
 
 class TestTodo:
 
     RESPONSE_TIMEOUT = 5
+
+    start_url = "http://127.0.0.1:8000"
 
     @classmethod
     def __request(cls, url, method="GET", json_data=None):
@@ -25,4 +29,14 @@ class TestTodo:
             print(f'{exception = }')
 
     def test_retrieve_todos(self):
-        pass
+        response = self.__request(url=f"{self.start_url}/todo")
+        assert type(response['todos']) == list
+
+    def test_add_todo(self):
+        data = {"id": 1, "item": "First Todo is to finish this book!"}
+        response = self.__request(url=f"{self.start_url}/todo", method="POST", json_data=data)
+        assert response['message'] == 'TODO added successfully'
+
+        response = self.__request(url=f"{self.start_url}/todo")
+        assert len(response['todos']) == 1
+        assert response['todos'] == [{'id': 1, 'item': 'First Todo is to finish this book!'}]
