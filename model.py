67db205695c5bc0fb36f8e23ea2cbf95fe5726e9
SKolodkin <dev1@instrument-fit.ru>
2023-11-29 08:54:56 +0300

    add templates

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
-from typing import List
+from typing import List, Optional
 
+from fastapi import Form
 from pydantic import BaseModel
 
 
 class Todo(BaseModel):
-    id: int
+    id:   Optional[int]
     item: str
+
+    @classmethod
+    def as_form(cls, item: str = Form(...)):
+        return cls(item=item)
 
     class Config:
         json_schema_extra = {
--- todo.py
+++ todo.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Path, HTTPException, status
+from fastapi import APIRouter, Path, HTTPException, status, Depends, Request
+from fastapi.templating import Jinja2Templates
 
 from model import Todo, TodoItem, TodoItems
 
@@ -7,23 +8,26 @@ todo_router = APIRouter()
 
 todo_list = []
 
+templates = Jinja2Templates(directory="templates/")
+
 
 @todo_router.get("/todo", response_model=TodoItems)
-async def retrieve_todos() -> dict:
-    return {"todos": todo_list}
+async def retrieve_todos(request: Request):
+    return templates.TemplateResponse("todo.html", {"request": request, "todos": todo_list})
 
 
 @todo_router.post("/todo", status_code=201)
-async def add_todo(todo: Todo) -> dict:
+async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)):
+    todo.id = len(todo_list) + 1
     todo_list.append(todo)
-    return {"message": "TODO added successfully"}
+    return templates.TemplateResponse("todo.html", {"request": request, "todos": todo_list})
 
 
 @todo_router.get("/todo/{todo_id}")
-async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+async def get_single_todo(request: Request, todo_id: int = Path(..., title="The ID of the todo to retrieve.")):
     for todo in todo_list:
         if todo.id == todo_id:
-            return {"todo": todo}
+            return templates.TemplateResponse("todo.html", {"request": request, "todos": todo_list})
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Todo with supplied ID doesn't exist",
